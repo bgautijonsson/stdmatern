8c88c0de8107f46a9632cda85b36bd28efa38d00
bgautijonsson <bgautijonsson@gmail.com>
2024-08-14 09:11:07 +0000

    Updating the C++ code.

--- benchmark/circulant.R
+++ benchmark/circulant.R
@@ -1,60 +1,76 @@
-dim <- 5
-rho <- 0.5
+library(stdmatern)
+library(tidyverse)
+library(ggh4x)
+theme_set(bggjphd::theme_bggj())
 
-base_Q1 <- numeric(dim)
-base_Q1[1] <- 1 + rho^2
-base_Q1[c(2, dim)] <- -rho
-
-Q1 <- matrix(
-  0,
-  nrow = dim,
-  ncol = dim
-)
-
-for (i in 1:dim) {
-  Q1[i, (i + 1:dim - 2) %% dim + 1] <- base_Q1
-}
-
-I <- diag(1, nrow = dim, ncol = dim)
-
-Q <- kronecker(Q1, I) + kronecker(I, Q1)
+dim <- 20
+rho <- 0.8
+nu <- 2
+n_obs <- 4000
 
 
+exact <- cor(t(rmatern_copula(n_obs, dim, rho, nu)))[1, ]
+circulant <- cor(t(rmatern_copula_circulant(n_obs, dim, rho, nu)))[1, ]
+folded <- cor(t(rmatern_copula_folded_full(n_obs, dim, rho, nu)))[1, ]
 
-base_Q <- matrix(
-  0,
-  nrow = dim,
-  ncol = dim
-)
+tibble(
+  Exact = exact,
+  Circulant = circulant,
+  Folded = folded
+) |> 
+  mutate(
+    index = row_number()
+  ) |> 
+  pivot_longer(c(-index)) |> 
+  ggplot(aes(index, value, col = name)) +
+  geom_line() +
+  scale_x_continuous(
+    guide = guide_axis_truncated()
+  ) +
+  scale_y_continuous(
+    breaks = seq(0, 1, by = 0.1),
+    guide = guide_axis_truncated()
+  ) +
+  scale_colour_brewer(
+    palette = "Set1"
+  ) +
+  labs(
+    title = "Comparing the first line in the correlation matrices for each method",
+    subtitle = "Shown for rho = 0.8, nu = 2 and dim = 20",
+    x = NULL,
+    y = "Correlation",
+    col = NULL
+  ) +
+  theme(
+    legend.position = "top"
+  )
 
-base_Q[1, ] <- base_Q1 + c(1 + rho^2, rep(0, dim - 1)) 
-base_Q[c(2, dim), 1] <- -rho
 
-fft(base_Q)
 
-mean(abs(sort(Re(fft(base_Q)), decreasing = TRUE) - eigen(Q)$values))
 
-Q_inv <- solve(Q)
 
-temp <- base_Q
+  dim <- 40
+  rho <- 0.8
+  nu <- 1
 
-for (i in seq_len(dim)) {
-  temp[i, ] <- Q_inv[(i - 1) * dim + 1, seq_len(dim)]
-}
+rmatern_copula(1, dim, rho, nu) |> 
+  pnorm() |> 
+  evd::qgev(loc = 10, scale = 5, shape = 0.1) |> 
+  as.numeric() |> 
+  tibble() |> 
+  rename(
+    y = 1
+  ) |> 
+  mutate(
+    id = row_number(),
+    lat = (id - 1) %% dim,
+    lon = cumsum(lat == 0)
+  ) |> 
+  ggplot(aes(lat, lon)) +
+  geom_raster(aes(fill = y))
 
-#temp
-Re(fft(1 / fft(base_Q), inverse = TRUE) / dim^2)
 
-mean(abs(temp - Re(fft(1 / fft(base_Q), inverse = TRUE) / dim^2)))
 
-base_C <- base_Q
 
-eigenvalues <- fft(base_C)
-
-base_C_inverse <- fft(1 / eigenvalues, inverse = TRUE) / dim^2
-
-mvar <- base_C_inverse[1, 1]
-
-updated_eigenvalues <- mvar * eigenvalues
 
 
